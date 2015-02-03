96bb1136cf7b668caf39171bd8b4ad0ed3db1f89
Murray King <murray.king@ed.ac.uk>
2015-02-03 16:45:59 +0000

    Add more iso elements

--- www/viewer/js/modules/parseiso.js
+++ www/viewer/js/modules/parseiso.js
@@ -2,69 +2,83 @@
 /*global $, define, console */
 
 define(['jquery', 'jqueryxpath', 'underscore'], function () {
+    'use strict';
+
+
 
     var namespaces = {
-        None: 'http://www.isotc211.org/2005/gmd',
-        'gco': 'http://www.isotc211.org/2005/gco',
-        'gmd': 'http://www.isotc211.org/2005/gmd',
-        'gml': 'http://www.opengis.net/gml',
-        'gml32': 'http://www.opengis.net/gml/3.2',
-        'gmx': 'http://www.isotc211.org/2005/gmx',
-        'gts': 'http://www.isotc211.org/2005/gts',
-        'srv': 'http://www.isotc211.org/2005/srv',
-        'xlink': 'http://www.w3.org/1999/xlink',
-        'csw': "http://www.opengis.net/cat/csw/2.0.2"
-    };
+            None: 'http://www.isotc211.org/2005/gmd',
+            'gco': 'http://www.isotc211.org/2005/gco',
+            'gmd': 'http://www.isotc211.org/2005/gmd',
+            'gml': 'http://www.opengis.net/gml',
+            'gml32': 'http://www.opengis.net/gml/3.2',
+            'gmx': 'http://www.isotc211.org/2005/gmx',
+            'gts': 'http://www.isotc211.org/2005/gts',
+            'srv': 'http://www.isotc211.org/2005/srv',
+            'xlink': 'http://www.w3.org/1999/xlink',
+            'csw': "http://www.opengis.net/cat/csw/2.0.2"
+        },
 
 
-    var mappings = {
-        "identifier": "gmd:fileIdentifier/gco:CharacterString",
+        mappings = {
+            "identifier": "gmd:fileIdentifier/gco:CharacterString",
+            "title": "gmd:identificationInfo/gmd:MD_DataIdentification/gmd:citation/gmd:CI_Citation/gmd:title/gco:CharacterString",
+            "alternateTitle": "gmd:identificationInfo/gmd:MD_DataIdentification/gmd:citation/gmd:CI_Citation/gmd:alternateTitle",
+            "abstract": "gmd:identificationInfo/gmd:MD_DataIdentification/gmd:abstract/gco:CharacterString",
+            "date": "gmd:identificationInfo/gmd:MD_DataIdentification/gmd:citation/gmd:CI_Citation/gmd:date/gmd:CI_Date/gmd:date/gco:Date",
+            "dateType": "gmd:identificationInfo/gmd:MD_DataIdentification/gmd:citation/gmd:CI_Citation/gmd:date/gmd:CI_Date/gmd:dateType",
+            "uniqueResourceIdentifier": "gmd:identificationInfo/gmd:MD_DataIdentification/gmd:citation/gmd:CI_Citation/gmd:identifier/gmd:MD_Identifier/gmd:code/gco:CharacterString",
+            "codeSpace": "gmd:identificationInfo/gmd:MD_DataIdentification/gmd:citation/gmd:CI_Citation/gmd:identifier/gmd:MD_Identifier/gmd:codeSpace/gco:CharacterString",
+            "language": "gmd:language/gmd:LanguageCode/@codeListValue",
+            "keywords": "gmd:identificationInfo/gmd:MD_DataIdentification/gmd:descriptiveKeywords/gmd:MD_Keywords/gmd:keyword",
+            "parentidentifier": "gmd:parentIdentifier/gco:CharacterString",
+            "dataseturi": "gmd:dataSetURI/gco:CharacterString",
+            "languagecode": "gmd:language/gmd:LanguageCode",
+            "datestamp": "gmd:dateStamp/gco:Date or gmd:dateStamp/gco:DateTime",
+            "charset": "gmd:characterSet/gmd:MD_CharacterSetCode/@codeListValue",
+            "hierarchy": "gmd:hierarchyLevel/gmd:MD_ScopeCode/@codeListValue",
+            "datetimestamp": "gmd:dateStamp/gco:DateTime"
 
-        "language": "gmd:language/gmd:LanguageCode/@codeListValue",
-        "keywords": "gmd:identificationInfo/gmd:MD_DataIdentification/gmd:descriptiveKeywords/gmd:MD_Keywords/gmd:keyword",
-        "parentidentifier": "gmd:parentIdentifier/gco:CharacterString",
-        "dataseturi": "gmd:dataSetURI/gco:CharacterString",
-        "languagecode": "gmd:language/gmd:LanguageCode",
-        "datestamp": "gmd:dateStamp/gco:Date or gmd:dateStamp/gco:DateTime",
-        "charset": "gmd:characterSet/gmd:MD_CharacterSetCode/@codeListValue",
-        "hierarchy": "gmd:hierarchyLevel/gmd:MD_ScopeCode/@codeListValue",
-        "datetimestamp": "gmd:dateStamp/gco:DateTime"
-
-    };
+        },
 
 
-    var isoNamespaces = function (prefix) {
-        var ns = namespaces[prefix];
-        return ns;
 
-    };
+        isoNamespaces = function (prefix) {
+            var ns = namespaces[prefix];
+            return ns;
 
-    function getValueFromXPath(path, xml) {
+        },
 
-        var xpathResult = $(xml).xpath(path, isoNamespaces);
-        var arr = [];
-        xpathResult.each(function () {
-            arr.push($.trim(this.textContent));
+        getValueFromXPath = function (path, xml) {
 
-        });
-        if (arr.length === 1) {
-            return arr[0];
-        } else {
-            return arr;
-        }
+            var xpathResult = $(xml).xpath(path, isoNamespaces),
+                arr = [];
+            xpathResult.each(function () {
+                arr.push($.trim(this.textContent));
 
-    }
-    var buildIsoDoc = function (xml) {
-        var MD_metadata_element = $(xml).xpath('/csw:GetRecordByIdResponse/gmd:MD_Metadata', isoNamespaces);
-        var isoModel = {};
-        for (var key in mappings) {
-            console.log(key);
-            console.log(mappings[key]);
-            isoModel[key] = getValueFromXPath(mappings[key], MD_metadata_element);
+            });
+            if (arr.length === 1) {
+                return arr[0];
+            } else {
+                return arr;
+            }
+
+        },
+
+        buildIsoDoc = function (xml) {
+            var MD_metadata_element = $(xml).xpath('/csw:GetRecordByIdResponse/gmd:MD_Metadata', isoNamespaces),
+
+                isoModel = {},
+                key;
+            for (key in mappings) {
+                console.log(key);
+                console.log(mappings[key]);
+                isoModel[key] = getValueFromXPath(mappings[key], MD_metadata_element);
+            }
+            return isoModel;
+
         };
-        return isoModel;
 
-    };
     return {
 
         buildIsoDoc: buildIsoDoc
--- www/viewer/js/modules/tests.js
+++ www/viewer/js/modules/tests.js
@@ -10,20 +10,25 @@ define(['modules/model', 'jquery'], function (model) {
         var url = 'http://localhost/www/viewer/tests/testdoc.xml';
         test('test parse xml', function (assert) {
 
-            assert.expect(1);
+            assert.expect(4);
             var done1 = assert.async();
             $.ajax({
                 type: "GET",
                 url: url,
                 dataType: "xml",
                 success: function (xml) {
-                    model.createRecordDetailsModel(xml);
+                    var m = model.createRecordDetailsModel(xml);
                     assert.ok(xml, 'got xml');
 
+                    assert.equal(m.date, "1990", "date");
+                    assert.equal(m.title, "Index To Specimens Transferred From The John Smith Collection To The UK (North) Type and Stratigraphical Collection", "title");
+                    assert.equal(m.dateType, "creation", "dateType");
+                    assert.equal(m.uniqueResourceIdentifier, "http://data.bgs.ac.uk/id/dataHolding/13480091", "uniqueResourceIdentifier");
+                    assert.equal(m.codeSpace, "", "codeSpace");
                     done1();
                 },
-                error: function () {
-                    alert("An error occurred while processing XML file.");
+                error: function (jqXHR, textStatus, errorThrow) {
+                    alert("An error occurred while processing XML file." + errorThrow);
                 }
             });
 

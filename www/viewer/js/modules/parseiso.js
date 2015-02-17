f49bcfec9f5b82cc7c7361b69eb527c171ebf21f
Murray King <murray.king@ed.ac.uk>
2015-02-17 13:49:37 +0000

    tidy up a bit

--- www/viewer/js/modules/parseiso.js
+++ www/viewer/js/modules/parseiso.js
@@ -72,109 +72,88 @@ define(['modules/metadata_mapping', 'jquery', 'jqueryxpath', 'underscore'], func
 
         },
 
-        getValueFromXPath = function (cxt, path, xml, xpathFromCtx) {
+        getValueFromXPath = function (cxt, $xml, xpathFromCtx) {
             //debug
             cxt = cxt.slice(0, -1);
-            if (cxt.indexOf("gmd:dateStamp") != -1) {
-                console.log('debug');
-            }
-            if (true) {
-                var testArr = $(xml).xpath(cxt, isoNamespaces);
-                var b = [];
-                for (var i = 0; i < testArr.length; i++) {
-                    var res = $(testArr[i]).xpath(xpathFromCtx, isoNamespaces);
-                    var arr = [];
 
-                    res.each(function (index) {
-                        arr[index] = ($.trim(this.textContent));
-                    });
-                    console.log(res);
-                    if (arr.length === 1) {
-                        b[i] = arr[0];
-                    } else {
-                        b[i] = arr;
-                    }
+            var elems = $xml.xpath(cxt, isoNamespaces);
+            var b = [];
+            for (var i = 0; i < elems.length; i++) {
+                var res = $(elems[i]).xpath(xpathFromCtx, isoNamespaces);
+                var arr = [];
+
+                res.each(function (index) {
+                    arr[index] = ($.trim(this.textContent));
+                });
+                console.log(res);
+                if (arr.length === 1) {
+                    b[i] = arr[0];
+                } else {
+                    b[i] = arr;
                 }
-                return b;
             }
+            return b;
 
-            var xpathResult = $(xml).xpath(path, isoNamespaces),
-                arr = [];
-            xpathResult.each(function () {
-                arr.push($.trim(this.textContent));
-
-            });
-            if (arr.length === 1) {
-                return arr[0];
-            } else {
-                return arr;
-            }
 
         },
 
         buildIsoDoc = function (xml) {
             var isoModel = {},
-                xpathOr = " or ";
+                xpathOr = " or ",
+                $xml = $(xml);
             recursiveParse(isoModel, mappings, '/csw:GetRecordByIdResponse/gmd:MD_Metadata/');
 
-            function recursiveParse(model, map, context, xpathContext) {
-                var key;
-                if (map.hasOwnProperty('context')) {
-                    //what if context has or xpath operator;
-                    if (map.context.indexOf(xpathOr) > -1) {
-                        context = handleXpathOrOperationInContext(map.context, context);
-                    } else {
-                        context = context + map.context;
-
-
-
-                    }
-                    context = context + '/';
-                }
-
-                for (key in map) {
-                    //skip context nodes
-                    if (key === 'context') {
-                        continue;
-                    }
-                    if (map.hasOwnProperty(key)) {
-                        var prop = map[key];
-                        //debug
-                        if (key == 'thesaurus') {
-                            console.log(key);
-                        }
-
-                        if (!isObject(prop)) {
-                            var xpath = map[key];
-
-                            //test expression has an operator as it doesn't work in this impl
-                            var xpathResult = undefined;
-                            if (xpath.indexOf(xpathOr) > -1) {
-                                xpathResult = handleXpathOrOperation(xpath, context);
-                            } else {
-                                xpathResult = getValueFromXPath(context, context + xpath, xml, xpath);
-                                console.log(xpath);
-                            }
-
-                            model[key] = xpathResult;
-
+            function recursiveParse(model, map, context) {
+                    var key;
+                    if (map.hasOwnProperty('context')) {
+                        //what if context has or xpath operator;
+                        if (map.context.indexOf(xpathOr) > -1) {
+                            context = handleXpathOrOperationInContext(map.context, context);
                         } else {
-                            var details = {};
-                            if (key !== 'value') {
+                            context = context + map.context;
+                        }
+                        context = context + '/';
+                    }
 
-                                model[key] = details;
-
-                            } else {
-                                details = model;
+                    for (key in map) {
+                        //skip context nodes
+                        if (key === 'context') {
+                            continue;
+                        }
+                        if (map.hasOwnProperty(key)) {
+                            var prop = map[key];
+                            //debug
+                            if (key == 'thesaurus') {
+                                console.log(key);
                             }
 
+                            if (!isObject(prop)) {
+                                var xpath = map[key];
 
-                            recursiveParse(details, prop, context);
+
+                                var xpathResult = getValueFromXPath(context, $xml, xpath);
+                                console.log(xpath);
+
+
+                                model[key] = xpathResult;
+
+                            } else {
+                                var details = {};
+                                if (key !== 'value') {
+
+                                    model[key] = details;
+
+                                } else {
+                                    details = model;
+                                }
+
+
+                                recursiveParse(details, prop, context);
+                            }
                         }
                     }
                 }
-            }
-
+                //pain test expression has an 'OR' operator as it doesn't work in this impl
             function handleXpathOrOperationInContext(xpath, context) {
                 var xpaths = xpath.split(" or "),
                     emptyResult = function (res) {
@@ -190,21 +169,6 @@ define(['modules/metadata_mapping', 'jquery', 'jqueryxpath', 'underscore'], func
 
                 }
                 return context + xpaths[0];
-            }
-
-            function handleXpathOrOperation(xpath, context) {
-                var xpaths = xpath.split(" or "),
-                    emptyResult = function (res) {
-                        return !res || (res.constructor === Array && res.length === 0);
-                    },
-                    xpathResult = getValueFromXPath(context, context + xpaths[0], xml, xpaths[0]);
-                // if empty result try othe path
-                if (emptyResult(xpathResult)) {
-
-                    xpathResult = getValueFromXPath(context, context + xpaths[1], xml, xpaths[1]);
-
-                }
-                return xpathResult;
             }
 
             return isoModel;
